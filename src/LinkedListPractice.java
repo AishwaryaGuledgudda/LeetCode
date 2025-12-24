452f09242f8316c5f924b7226ab4857da52f1cca
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-12-24 16:27:42 +0000

    LeetCode journey

--- src/LinkedListPractice.java
+++ src/LinkedListPractice.java
@@ -11,8 +11,9 @@ public class LinkedListPractice {
         }
 
     }
-    static Node head;
-    static  Node tail;
+    Node head;
+    Node tail;
+    int size=0;
 
     public void insertFirst(int val){
         Node node = new Node(val);
@@ -21,6 +22,8 @@ public class LinkedListPractice {
         if(tail==null){
             tail=head;
         }
+
+        size++;
     }
 
     public void display(){
@@ -30,6 +33,83 @@ public class LinkedListPractice {
             temp=temp.next;
         }
         System.out.println("End");
+        System.out.println("The size of the Linked-List is: "+size);
+    }
+
+    public void insertLast(int val){
+        if(tail==null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail=node;
+
+        size++;
+    }
+
+    public void insertIndex(int val, int index){
+        if(index==0){
+            insertFirst(val);
+            return;
+        }
+        if(index==size){
+            insertLast(val);
+            return;
+        }
+
+        Node temp=head;
+        for(int i=1;i<index;i++){
+            temp=temp.next;
+        }
+
+        Node node = new Node(val);
+        Node address = temp.next;
+        temp.next=node;
+        node.next=address;
+        size++;
+
+    }
+
+    public int deleteFirst(){
+        int val=head.val;
+        head=head.next;
+        if(head==null){
+            tail=null;
+        }
+        size--;
+        return val;
+    }
+
+    public int deleteLast(){
+        Node temp=head;
+        for(int i=1;i<size-1;i++){
+            temp=temp.next;
+        }
+
+        int val= tail.val;
+        tail=temp;
+        tail.next=null;
+        size--;
+        return val;
+
+    }
+
+    public int deleteIndex(int index){
+        Node prev=head;
+        if(index==0){
+            return deleteFirst();
+        }
+        if(index==size-1){
+            return deleteLast();
+        }
+
+        for(int i=0;i<index-1;i++){
+            prev=prev.next;
+        }
+        int val = prev.next.val;
+        prev.next = prev.next.next;
+        return val;
     }
 
 
@@ -40,6 +120,13 @@ public class LinkedListPractice {
         llp.insertFirst(6);
         llp.insertFirst(9);
         llp.insertFirst(3);
+        llp.insertLast(10);
+        llp.insertIndex(69,3);
+        //llp.display();
+//        System.out.println("The deleted number is: "+llp.deleteFirst());
+//        System.out.println("The deleted number in last is: "+llp.deleteLast());
+        System.out.println("The deleted num in index location is:" +llp.deleteIndex(3));
         llp.display();
+
     }
 }
