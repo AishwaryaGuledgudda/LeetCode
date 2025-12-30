33ff4fbe2b23b59d96954c2532598b9f7e5099ca
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-12-30 17:15:23 +0000

    LeetCode journey

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,11 @@
+import com.sun.source.tree.WhileLoopTree;
+
+public class LinkedListCycle extends LinkedListPractice {
+
+    public static void main(String[] args) {
+
+    }
+   //Check out LinkedListPractice.LinkedListCycle() program
+
+
+}
--- src/LinkedListPractice.java
+++ src/LinkedListPractice.java
@@ -112,6 +112,23 @@ public class LinkedListPractice {
         return val;
     }
 
+    //LeetCode
+    public boolean LinkedListCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(slow!=null&&fast!=null&&fast.next!=null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
     public static void main(String[] args) {
